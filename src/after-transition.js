8306706e32823a80ab30f5a0793e07df0656a7c7
Dor Tzur <dor@spot.im>
2017-01-17 10:46:51 +0200

    remove transition check

--- src/after-transition.js
+++ src/after-transition.js
@@ -1,18 +1,17 @@
-var hasTransitions = require('./has-transition');
 var emitter = require('component-css-emitter');
 
 function afterTransition(el, callback) {
-  if(hasTransitions(el)) {
+    // if(true) {
     return emitter(el).bind(callback);
-  }
-  return callback.apply(el);
+    // }
+    // return callback.apply(el);
 };
 
-afterTransition.once = function(el, callback) {
-  afterTransition(el, function fn(){
-    callback.apply(el);
-    emitter(el).unbind(fn);
-  });
+afterTransition.once = function (el, callback) {
+    afterTransition(el, function fn() {
+        callback.apply(el);
+        emitter(el).unbind(fn);
+    });
 };
 
 module.exports = afterTransition;
